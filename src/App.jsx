ce5c742bbb0c78f750ef03b6439488c9abd9e89c
LUCASREDBULL02 <lucas.alm@hotmail.se>
2025-12-11 20:59:58 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo, useEffect } from "react";
-import "./src/index.css";
+import "./index.css";
 
 import ProfileView from "./components/ProfileView.jsx";
 import LiftTools from "./components/LiftTools.jsx";
